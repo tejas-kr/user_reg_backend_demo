69dfcf0cfa1e31e2b8205a3891f88f2050fb8f86
Tejas K Jaiswal <tejasjaiswal9711@gmail.com>
2024-10-20 19:01:42 +0530

    added cors

--- api/main.py
+++ api/main.py
@@ -1,7 +1,9 @@
+import os
 import uvicorn
 from contextlib import asynccontextmanager
 from typing import AsyncGenerator
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from sqlmodel import SQLModel
 from .utils.log_util import logger
 from .utils.db_utils import engine
@@ -20,6 +22,16 @@ async def lifespan(app: FastAPI) -> AsyncGenerator:
 
 
 app = FastAPI(lifespan=lifespan)
+
+origins: list = os.getenv("CROSS_ORIGINS", "").split(';')
+logger.info(f"ORIGINS: {origins}")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # List of allowed origins
+    allow_credentials=True,  # Allow cookies to be sent cross-origin
+    allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all custom headers
+)
 
 app.include_router(users.router)
 app.include_router(books.router)
